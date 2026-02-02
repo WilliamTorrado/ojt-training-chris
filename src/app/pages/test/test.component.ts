34a54ee0911c86434c5f160fba6f0bcd54451073
Jerald Bon Harris Agustin <162771035+jrldbnhrrs@users.noreply.github.com>
2026-02-02 15:41:38 +0800

    my first commit

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  user = {
+    title: 'internship',
+    name: 'jerald bon harris',
+    status: 'online'
+  };
 
-  ngOnInit() {
+  allowClick = false;
+  showTasks = false;
+
+  currentDate = new Date();
+
+  taskList = [
+    { id: 1, description: 'use interpolation', priority: 'high', deadline: new Date(2026, 1, 6) },
+    { id: 2, description: 'use ngif and ngfor', priority: 'medium', deadline: new Date(2026, 1, 5) },
+    { id: 3, description: 'use builtin pipes', priority: 'low', deadline: new Date(2026, 1, 4) }
+  ];
+
+  constructor() {
+   setTimeout(() => {
+     this.allowClick = true;
+   }, 2000);
   }
 
+  ngOnInit() {}
+
+  getStatus() { 
+    return this.user.status; 
+  }
+
+  onSave() {
+    alert('data saved successfully');
+  }
+
+  toggleTasks() {
+    this.showTasks = !this.showTasks;
+  }
+
+ 
 }
+
+
+ 
